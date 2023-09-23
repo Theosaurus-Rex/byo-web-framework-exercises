01dd4e8fb858325264a7383b9a42af303f4929f6
Theo Harris <theosaurus13@gmail.com>
2023-09-23 10:56:35 +1000

    Create a fully dynamic route that greets a user based on a greeting param and a name endpoint

--- cowboy_example/lib/cowboy_example/router.ex
+++ cowboy_example/lib/cowboy_example/router.ex
@@ -1,10 +1,9 @@
 defmodule CowboyExample.Router do
   @moduledoc """
   This module defines all the routes, params, and handlers.
-
-  This module is also the handler module for the root route.
   """
   require Logger
+  alias CowboyExample.Router.Handlers.{Root, Greet}
 
   @doc """
   Returns the list of routes configured by this web server
@@ -12,24 +11,11 @@ defmodule CowboyExample.Router do
   def routes do
     [
       # For now, this module itself will handle root requests
-      {:_, [{"/", __MODULE__, []}]}
+      {:_,
+       [
+         {"/", Root, []},
+         {"/greet/:who", [who: :nonempty], Greet, []}
+       ]}
     ]
-  end
-
-  @doc """
-  This function handles the root route, logs the requests and responds with Hello World as the body.
-  """
-  def init(req0, state) do
-    Logger.info("Received request: #{inspect(req0)}")
-
-    req1 =
-      :cowboy_req.reply(
-        200,
-        %{"content-type" => "text/html"},
-        "Hello World",
-        req0
-      )
-
-    {:ok, req1, state}
   end
 end
