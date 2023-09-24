16f29e1ec6fe4b57933601d520b2bb4d4dd8a126
Theo Harris <theosaurus13@gmail.com>
2023-09-24 10:14:47 +1000

    Restrict routes for which experiment_server returns a successful response

--- experiment_server.exs
+++ experiment_server.exs
@@ -18,20 +18,32 @@ defmodule ExperimentServer do
     {:ok, connection_sock} = :gen_tcp.accept(listen_socket)
     {:ok, req} = :gen_tcp.recv(connection_sock, 0)
 
+    {_http_req, method, {_type, path}, _v} = req
+
     Logger.info("Got request: #{inspect(req)}")
-    respond(connection_sock)
+    respond(connection_sock, {method, path})
     listen(listen_socket)
   end
 
-  defp respond(connection_sock) do
-    # Send a proper HTTP/1.1 response with status
-    response = http_1_1_response("Hello World", 200)
+  defp respond(connection_sock, route) do
+    response = get_response(route)
     :gen_tcp.send(connection_sock, response)
 
     Logger.info("Sent response")
 
     :gen_tcp.close(connection_sock)
   end
+
+  defp get_response(route) do
+    {body, status} = body_and_status_for(route)
+    http_1_1_response(body, status)
+  end
+
+  defp body_and_status_for({:GET, "/hello"}) do
+    {"Hello World", 200}
+  end
+
+  defp body_and_status_for(_route), do: {"Not Found", 404}
 
   # Converts a body to HTTP/1.1 response string
   defp http_1_1_response(body, status) do
