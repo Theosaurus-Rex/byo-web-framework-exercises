49feb3f906c9f87d5ebe2193f509dac07bcc5b22
Theo Harris <theosaurus13@gmail.com>
2023-10-15 12:55:08 +1100

    Remove goldcrest_http_server for fresh start

--- goldcrest/goldcrest_http_server/lib/goldcrest_http_server/responder.ex
+++
@@ -1,5 +0,0 @@
-defmodule Goldcrest.HTTPServer.Responder do
-  @type method :: :GET | :POST | :PUT | :PATCH | :DELETE
-
-  @callback resp(term(), method(), String.t()) :: Goldcrest.HTTPResponse.t()
-end
